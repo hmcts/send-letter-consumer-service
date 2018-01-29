699e3981de3c82733939a3f34a50b8209663e461
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2018-01-29 17:25:28 +0100

    Use service name in pdf file name (#7)

--- src/main/java/uk/gov/hmcts/reform/slc/model/Letter.java
+++ src/main/java/uk/gov/hmcts/reform/slc/model/Letter.java
@@ -9,15 +9,18 @@ public class Letter {
     public final String template;
     public final Map<String, Object> values;
     public final String type;
+    public final String service;
 
     public Letter(
         @JsonProperty("template") String template,
         @JsonProperty("values") Map<String, Object> values,
-        @JsonProperty("type") String type
+        @JsonProperty("type") String type,
+        @JsonProperty("service") String service
     ) {
         this.template = template;
         this.values = values;
         this.type = type;
+        this.service = service;
     }
 
     @Override
@@ -26,6 +29,7 @@ public class Letter {
             + "template='" + template + '\''
             + ", values=" + values
             + ", type='" + type + '\''
+            + ", service='" + service + '\''
             + '}';
     }
 }
