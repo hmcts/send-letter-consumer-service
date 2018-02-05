2a3eeb9a515015b62fa1dbe49aa3ab1b99d15b17
donatas M <doncem@users.noreply.github.com>
2018-02-05 17:09:34 +0000

    Update title of a test case

--- src/test/java/uk/gov/hmcts/reform/slc/services/steps/maptoletter/LetterMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/slc/services/steps/maptoletter/LetterMapperTest.java
@@ -57,7 +57,7 @@ public class LetterMapperTest {
     }
 
     @Test
-    public void should_throw_an_exception_if_message_contains_null_fields() {
+    public void should_throw_an_exception_if_message_contains_empty_body_representing_all_fields_as_null() {
         given(message.getBody()).willReturn("{}".getBytes());
 
         assertThatThrownBy(() -> letterMapper.from(message))
