cea209c6b2860b84f7317c6e324989a6270f8106
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-02-05 15:07:51 +0000

    Ignore vertical alignment

--- src/main/java/uk/gov/hmcts/reform/slc/logging/AppDependency.java
+++ src/main/java/uk/gov/hmcts/reform/slc/logging/AppDependency.java
@@ -6,37 +6,37 @@ final class AppDependency {
      * Service bus message receiver dependency.
      * Used to track dependency of how long in millis did it take to...
      */
-    static final String MESSAGE_RECEIVER_DEPENDENCY     = "MessageReceiver";
+    static final String MESSAGE_RECEIVER_DEPENDENCY = "MessageReceiver";
 
     /**
      * .. receive message receive from service bus queue.
      */
-    static final String MESSAGE_RECEIVED_COMMAND        = "MessageReceived";
+    static final String MESSAGE_RECEIVED_COMMAND = "MessageReceived";
 
     /**
      * .. release lock and remove message from service bus queue.
      */
-    static final String MESSAGE_COMPLETED_COMMAND       = "MessageCompleted";
+    static final String MESSAGE_COMPLETED_COMMAND = "MessageCompleted";
 
     /**
      * .. release lock and send message to dead letter pool.
      */
-    static final String MESSAGE_DEAD_LETTER_COMMAND     = "MessageDeadLettered";
+    static final String MESSAGE_DEAD_LETTER_COMMAND = "MessageDeadLettered";
 
     /**
      * Pdf service client.
      * Used to track dependency of how long in millis did it take to...
      */
-    static final String PDF_GENERATOR_DEPENDENCY        = "PdfGenerator";
+    static final String PDF_GENERATOR_DEPENDENCY = "PdfGenerator";
 
     /**
      * .. generate pdf.
      */
-    static final String GENERATE_PDF_FROM_HTML_COMMAND  = "GeneratePdfFromHtml";
+    static final String GENERATE_PDF_FROM_HTML_COMMAND = "GeneratePdfFromHtml";
 
-    static final String FTP_DEPENDENCY                  = "FtpClient";
+    static final String FTP_DEPENDENCY = "FtpClient";
 
-    static final String FTP_FILE_UPLOAD_COMMAND         = "FtpFileUpload";
+    static final String FTP_FILE_UPLOAD_COMMAND = "FtpFileUpload";
 
     private AppDependency() {
         // utility class constructor
