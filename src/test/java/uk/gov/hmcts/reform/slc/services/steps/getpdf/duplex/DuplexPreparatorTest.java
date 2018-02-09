fa3adf0b65b1835d02c4cb15e1b6ea0983d6c5b8
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2018-02-09 10:39:32 +0000

    Handle duplex printing (#25)

---
+++ src/test/java/uk/gov/hmcts/reform/slc/services/steps/getpdf/duplex/DuplexPreparatorTest.java
@@ -0,0 +1,38 @@
+package uk.gov.hmcts.reform.slc.services.steps.getpdf.duplex;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.junit.Test;
+
+import static com.google.common.io.Resources.getResource;
+import static com.google.common.io.Resources.toByteArray;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DuplexPreparatorTest {
+
+    @Test
+    public void should_add_blank_page_if_total_number_of_pages_is_odd() throws Exception {
+        // given
+        byte[] before = toByteArray(getResource("single_page.pdf"));
+
+        // when
+        byte[] after = new DuplexPreparator().prepare(before);
+
+        // then
+        assertThat(after).isNotEqualTo(before);
+        try (PDDocument pdDoc = PDDocument.load(after)) {
+            assertThat(pdDoc.getNumberOfPages()).as("number of pages").isEqualTo(2);
+        }
+    }
+
+    @Test
+    public void should_not_add_a_blank_page_if_total_number_of_pages_is_even() throws Exception {
+        // given
+        byte[] before = toByteArray(getResource("two_pages.pdf"));
+
+        // when
+        byte[] after = new DuplexPreparator().prepare(before);
+
+        // then
+        assertThat(after).isEqualTo(before);
+    }
+}
