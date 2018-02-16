5439d7a66943043d7b5f8858d9e8dbbb208db4b9
nitinprabhu <prabhunitin61@gmail.com>
2018-02-16 14:46:33 +0000

    Refactored file name generation logic

--- src/main/java/uk/gov/hmcts/reform/slc/services/steps/getpdf/FileNameGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/slc/services/steps/getpdf/FileNameGenerator.java
@@ -1,16 +1,16 @@
 package uk.gov.hmcts.reform.slc.services.steps.getpdf;
 
+import uk.gov.hmcts.reform.slc.model.Letter;
+
 import static org.apache.commons.codec.digest.DigestUtils.md5Hex;
 
 public final class FileNameGenerator {
 
     public static String generateFor(
-        String letterType,
-        String jurisdiction,
-        byte[] content,
+        Letter letter,
         String extension
     ) {
-        return letterType + "-" + jurisdiction + "-" + md5Hex(content) + "." + extension;
+        return letter.type + "-" + letter.service + "-" + md5Hex(letter.messageId) + "." + extension;
     }
 
     private FileNameGenerator() {
--- src/test/java/uk/gov/hmcts/reform/slc/services/steps/getpdf/FileNameGeneratorTest.java
+++ src/test/java/uk/gov/hmcts/reform/slc/services/steps/getpdf/FileNameGeneratorTest.java
@@ -1,7 +1,12 @@
 package uk.gov.hmcts.reform.slc.services.steps.getpdf;
 
 import org.junit.Test;
+import uk.gov.hmcts.reform.slc.model.Letter;
 
+import java.util.UUID;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.emptyMap;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FileNameGeneratorTest {
@@ -9,13 +14,10 @@ public class FileNameGeneratorTest {
     @Test
     public void should_generate_file_name_in_expected_format() {
         // given
-        String letterType = "typeA";
-        String jurisdiction = "cmc";
-        byte[] content = "hello".getBytes();
-        String extension = "pdf";
+        Letter letter = createLetter("typeA", "cmc", "098F6BCD4621D373CADE4E832627B4F6");
 
         // when
-        String result = FileNameGenerator.generateFor(letterType, jurisdiction, content, extension);
+        String result = FileNameGenerator.generateFor(letter, "pdf");
 
         // then
         assertThat(result).matches("typeA-cmc-.*\\.pdf");
@@ -23,20 +25,38 @@ public class FileNameGeneratorTest {
 
     @Test
     public void should_always_generate_the_same_name_for_same_input() {
+        // given
+        Letter letter1 = createLetter("A", "B", "098F6BCD4621D373CADE4E832627B4F6");
+        Letter letter2 = createLetter("A", "B", "098F6BCD4621D373CADE4E832627B4F6");
 
-        String result1 = FileNameGenerator.generateFor("A", "B", "C".getBytes(), "pdf");
-        String result2 = FileNameGenerator.generateFor("A", "B", "C".getBytes(), "pdf");
+        String result1 = FileNameGenerator.generateFor(letter1, "pdf");
+        String result2 = FileNameGenerator.generateFor(letter2, "pdf");
 
         // then
         assertThat(result1).isEqualTo(result2);
     }
 
     @Test
-    public void should_generate_different_names_for_different_file_contents() {
-        String result1 = FileNameGenerator.generateFor("A", "B", "some_content".getBytes(), "pdf");
-        String result2 = FileNameGenerator.generateFor("A", "B", "completely_different_content".getBytes(), "pdf");
+    public void should_generate_different_names_for_different_message_id() {
+        // given
+        Letter letter1 = createLetter("A", "B", "5A105E8B9D40E1329780D62EA2265D8A");
+        Letter letter2 = createLetter("A", "B", "AD0234829205B9033196BA818F7A872B");
+
+        String result1 = FileNameGenerator.generateFor(letter1, "pdf");
+        String result2 = FileNameGenerator.generateFor(letter2, "pdf");
 
         // then
         assertThat(result1).isNotEqualTo(result2);
+    }
+
+    private Letter createLetter(String type, String service, String messageId) {
+        return new Letter(
+            UUID.randomUUID(),
+            emptyList(),
+            type,
+            service,
+            messageId,
+            emptyMap()
+        );
     }
 }
