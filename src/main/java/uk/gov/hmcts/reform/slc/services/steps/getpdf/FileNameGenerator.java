33041500037b15e9ed0c8c132514a6581e114d2f
nitinprabhu <prabhunitin61@gmail.com>
2018-02-16 14:57:20 +0000

    Fixed review comment

--- src/main/java/uk/gov/hmcts/reform/slc/services/steps/getpdf/FileNameGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/slc/services/steps/getpdf/FileNameGenerator.java
@@ -2,15 +2,13 @@ package uk.gov.hmcts.reform.slc.services.steps.getpdf;
 
 import uk.gov.hmcts.reform.slc.model.Letter;
 
-import static org.apache.commons.codec.digest.DigestUtils.md5Hex;
-
 public final class FileNameGenerator {
 
     public static String generateFor(
         Letter letter,
         String extension
     ) {
-        return letter.type + "-" + letter.service + "-" + md5Hex(letter.messageId) + "." + extension;
+        return letter.type + "-" + letter.service + "-" + letter.messageId + "." + extension;
     }
 
     private FileNameGenerator() {
