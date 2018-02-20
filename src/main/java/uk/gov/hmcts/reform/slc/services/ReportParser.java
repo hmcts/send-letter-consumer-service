1034648342dc24ca7b6be197131dc4fc06f9c2e7
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2018-02-20 16:06:52 +0000

    Parse csv reports (#45)

---
+++ src/main/java/uk/gov/hmcts/reform/slc/services/ReportParser.java
@@ -0,0 +1,67 @@
+package uk.gov.hmcts.reform.slc.services;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.slc.model.LetterPrintStatus;
+import uk.gov.hmcts.reform.slc.services.steps.getpdf.FileNameHelper;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.StreamSupport.stream;
+
+@Component
+public class ReportParser {
+
+    private static final Logger logger = LoggerFactory.getLogger(ReportParser.class);
+
+    public List<LetterPrintStatus> parse(byte[] report) {
+        try (CSVParser parser = parserFor(report)) {
+
+            return stream(parser.spliterator(), false)
+                .map(row -> toPrintStatus(row))
+                .filter(status -> status != null)
+                .collect(toList());
+
+        } catch (IOException exc) {
+            throw new ReportParsingException(exc);
+        }
+    }
+
+    private CSVParser parserFor(byte[] csv) throws IOException {
+        return CSVFormat
+            .DEFAULT
+            .withHeader()
+            .parse(new InputStreamReader(new ByteArrayInputStream(csv)));
+    }
+
+    /**
+     * Converts cvs row into a letter print status object.
+     * Returns null if conversion fails.
+     */
+    private LetterPrintStatus toPrintStatus(CSVRecord record) {
+        try {
+            return new LetterPrintStatus(
+                FileNameHelper.extractId(record.get("Filename")),
+                ZonedDateTime.parse(record.get("Date") + "T" + record.get("Time") + "Z")
+            );
+        } catch (Exception exc) {
+            logger.error("Error parsing row: " + record, exc);
+            return null;
+        }
+    }
+
+    public static class ReportParsingException extends RuntimeException {
+        public ReportParsingException(Throwable cause) {
+            super(cause);
+        }
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/slc/services/ReportParserTest.java
@@ -0,0 +1,68 @@
+package uk.gov.hmcts.reform.slc.services;
+
+import org.junit.Test;
+import uk.gov.hmcts.reform.slc.model.LetterPrintStatus;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import static com.google.common.io.Resources.getResource;
+import static com.google.common.io.Resources.toByteArray;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ReportParserTest {
+
+    @Test
+    public void should_parse_valid_csv_report() {
+        String report =
+            "\"Date\",\"Time\",\"Filename\"\n"
+                + "2018-01-01,10:30:53,TE5A_TE5B_9364001\n"
+                + "2018-01-01,10:30:53,TE5A_TE5B_9364002\n";
+
+        List<LetterPrintStatus> result = new ReportParser().parse(report.getBytes());
+
+        assertThat(result)
+            .usingFieldByFieldElementComparator()
+            .containsExactlyInAnyOrder(
+                new LetterPrintStatus("9364001", ZonedDateTime.parse("2018-01-01T10:30:53Z")),
+                new LetterPrintStatus("9364002", ZonedDateTime.parse("2018-01-01T10:30:53Z"))
+            );
+    }
+
+    @Test
+    public void should_filter_out_rows_with_invalid_file_name() {
+        String report =
+            "\"Date\",\"Time\",\"Filename\"\n"
+                + "2018-01-01,10:30:53,invalidfilename\n"
+                + "2018-01-01,10:30:53,TE5A_TE5B_9364002\n";
+
+        List<LetterPrintStatus> result = new ReportParser().parse(report.getBytes());
+
+        assertThat(result)
+            .usingFieldByFieldElementComparator()
+            .containsExactly(new LetterPrintStatus("9364002", ZonedDateTime.parse("2018-01-01T10:30:53Z")));
+    }
+
+    @Test
+    public void should_filter_out_rows_with_invalid_date() {
+        String report =
+            "\"Date\",\"Time\",\"Filename\"\n"
+                + "20180101,10:30:53,TE5A_TE5B_9364001\n"
+                + "2018-01-01,10:30:53,TE5A_TE5B_9364002\n";
+
+        List<LetterPrintStatus> result = new ReportParser().parse(report.getBytes());
+
+        assertThat(result)
+            .usingFieldByFieldElementComparator()
+            .containsExactly(new LetterPrintStatus("9364002", ZonedDateTime.parse("2018-01-01T10:30:53Z")));
+    }
+
+    @Test
+    public void should_parse_sample_report() throws Exception {
+        byte[] report = toByteArray(getResource("report.csv"));
+
+        List<LetterPrintStatus> result = new ReportParser().parse(report);
+
+        assertThat(result).hasSize(11);
+    }
+}
