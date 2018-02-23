481438c1a72d9f856d2c588dda9e9268f3b90bcf
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-02-23 11:11:50 +0000

    Add all required environment keys

--- bin/run-in-docker.sh
+++ bin/run-in-docker.sh
@@ -14,7 +14,19 @@ print_help() {
     --help, -h                    Print this help block
 
   Available parameters:
-
+    APPLICATION_INSIGHTS_IKEY     Defaults to '00000000-0000-0000-0000-000000000000'
+    FTP_FINGERPRINT               Defaults to 'fingerprint'
+    FTP_HOSTNAME                  Defaults to 'hostname'
+    FTP_PORT                      Defaults to '22'
+    FTP_PRIVATE_KEY               Defaults to 'private'
+    FTP_PUBLIC_KEY                Defaults to 'public'
+    FTP_REPORTS_FOLDER            Defaults to '/reports/'
+    FTP_TARGET_FOLDER             Defaults to '/target/'
+    FTP_USER                      Defaults to 'user'
+    PDF_SERVICE_URL               Defaults to 'http://localhost:5500'
+    S2S_SECRET                    Defaults to 'secret'
+    S2S_URL                       Defaults to 'false' - disables health check
+    SEND_LETTER_PRODUCER_URL      Defaults to 'http://localhost:8485'
   "
 }
 
@@ -22,13 +34,20 @@ print_help() {
 GRADLE_CLEAN=false
 GRADLE_INSTALL=false
 
-# TODO custom environment variables application requires.
-# TODO also consider enlisting them in help string above ^
-# TODO sample: DB_PASSWORD   Defaults to 'dev'
 # environment variables
-#DB_PASSWORD=dev
-#S2S_URL=localhost
-#S2S_SECRET=secret
+APPLICATION_INSIGHTS_IKEY="00000000-0000-0000-0000-000000000000"
+FTP_FINGERPRINT="fingerprint"
+FTP_HOSTNAME="hostname"
+FTP_PORT=22
+FTP_PRIVATE_KEY="private"
+FTP_PUBLIC_KEY="public"
+FTP_REPORTS_FOLDER="/reports/"
+FTP_TARGET_FOLDER="/target/"
+FTP_USER="user"
+PDF_SERVICE_URL="http://localhost:5500"
+S2S_SECRET="secret"
+S2S_URL=false
+SEND_LETTER_PRODUCER_URL="http://localhost:8485"
 
 execute_script() {
   cd $(dirname "$0")/..
@@ -45,11 +64,21 @@ execute_script() {
     ./gradlew installDist
   fi
 
-#  echo "Assigning environment variables.."
-#
-#  export DB_PASSWORD=${DB_PASSWORD}
-#  export S2S_URL=${S2S_URL}
-#  export S2S_SECRET=${S2S_SECRET}
+  echo "Assigning environment variables.."
+
+  export APPLICATION_INSIGHTS_IKEY=${APPLICATION_INSIGHTS_IKEY}
+  export FTP_FINGERPRINT=${FTP_FINGERPRINT}
+  export FTP_HOSTNAME=${FTP_HOSTNAME}
+  export FTP_PORT=${FTP_PORT}
+  export FTP_PRIVATE_KEY=${FTP_PRIVATE_KEY}
+  export FTP_PUBLIC_KEY=${FTP_PUBLIC_KEY}
+  export FTP_REPORTS_FOLDER=${FTP_REPORTS_FOLDER}
+  export FTP_TARGET_FOLDER=${FTP_TARGET_FOLDER}
+  export FTP_USER=${FTP_USER}
+  export PDF_SERVICE_URL=${PDF_SERVICE_URL}
+  export S2S_SECRET=${S2S_SECRET}
+  export S2S_URL=${S2S_URL}
+  export SEND_LETTER_PRODUCER_URL=${SEND_LETTER_PRODUCER_URL}
 
   echo "Bringing up docker containers.."
 
@@ -63,9 +92,19 @@ while true ; do
     -i|--install) GRADLE_INSTALL=true ; shift ;;
     -p|--param)
       case "$2" in
-#        DB_PASSWORD=*) DB_PASSWORD="${2#*=}" ; shift 2 ;;
-#        S2S_URL=*) S2S_URL="${2#*=}" ; shift 2 ;;
-#        S2S_SECRET=*) S2S_SECRET="${2#*=}" ; shift 2 ;;
+        APPLICATION_INSIGHTS_IKEY=*) APPLICATION_INSIGHTS_IKEY="${2#*=}" ; shift 2 ;;
+        FTP_FINGERPRINT=*) FTP_FINGERPRINT="${2#*=}" ; shift 2 ;;
+        FTP_HOSTNAME=*) FTP_HOSTNAME="${2#*=}" ; shift 2 ;;
+        FTP_PORT=*) FTP_PORT="${2#*=}" ; shift 2 ;;
+        FTP_PRIVATE_KEY=*) FTP_PRIVATE_KEY="${2#*=}" ; shift 2 ;;
+        FTP_PUBLIC_KEY=*) FTP_PUBLIC_KEY="${2#*=}" ; shift 2 ;;
+        FTP_REPORTS_FOLDER=*) FTP_REPORTS_FOLDER="${2#*=}" ; shift 2 ;;
+        FTP_TARGET_FOLDER=*) FTP_TARGET_FOLDER="${2#*=}" ; shift 2 ;;
+        FTP_USER=*) FTP_USER="${2#*=}" ; shift 2 ;;
+        PDF_SERVICE_URL=*) PDF_SERVICE_URL="${2#*=}" ; shift 2 ;;
+        S2S_SECRET=*) S2S_SECRET="${2#*=}" ; shift 2 ;;
+        S2S_URL=*) S2S_URL="${2#*=}" ; shift 2 ;;
+        SEND_LETTER_PRODUCER_URL=*) SEND_LETTER_PRODUCER_URL="${2#*=}" ; shift 2 ;;
         *) shift 2 ;;
       esac ;;
     *) execute_script ; break ;;
