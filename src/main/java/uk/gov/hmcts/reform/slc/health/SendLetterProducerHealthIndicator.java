85c28a73a7a71bf5a8795a195195f281a5c62989
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-02-26 16:26:50 +0000

    Simplify send letter producer health indicator construct

--- src/main/java/uk/gov/hmcts/reform/slc/health/SendLetterProducerHealthIndicator.java
+++ src/main/java/uk/gov/hmcts/reform/slc/health/SendLetterProducerHealthIndicator.java
@@ -1,12 +1,12 @@
 package uk.gov.hmcts.reform.slc.health;
 
-import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.slc.services.SendLetterClient;
 
 @Component
-public class SendLetterProducerHealthIndicator extends AbstractHealthIndicator {
+public class SendLetterProducerHealthIndicator implements HealthIndicator {
 
     private final SendLetterClient client;
 
@@ -15,9 +15,7 @@ public class SendLetterProducerHealthIndicator extends AbstractHealthIndicator {
     }
 
     @Override
-    protected void doHealthCheck(Health.Builder builder) throws Exception {
-        Health health = client.serviceHealthy();
-        builder.status(health.getStatus());
-        health.getDetails().forEach(builder::withDetail);
+    public Health health() {
+        return client.serviceHealthy();
     }
 }
