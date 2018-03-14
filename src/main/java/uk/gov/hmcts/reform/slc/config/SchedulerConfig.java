3bbbdb09b5d9eff544eae0682f6cfd3794a79712
nitinprabhu <prabhunitin61@gmail.com>
2018-03-14 08:33:47 +0000

    Added task scheduler config

---
+++ src/main/java/uk/gov/hmcts/reform/slc/config/SchedulerConfig.java
@@ -0,0 +1,15 @@
+package uk.gov.hmcts.reform.slc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+
+@Configuration
+public class SchedulerConfig {
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ConcurrentTaskScheduler();
+    }
+}
