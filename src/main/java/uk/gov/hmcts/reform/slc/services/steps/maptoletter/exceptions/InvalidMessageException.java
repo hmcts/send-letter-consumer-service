8751d2820650d97b3de81a5d5ec1affbfb7ebf7c
donatas M <doncem@users.noreply.github.com>
2018-03-14 15:19:27 +0000

    Implement AbstractLoggingException (#95)
    * Implement AbstractLoggingException so log events are correct
    
    * Use 'unknown' error code instead of '0'

--- src/main/java/uk/gov/hmcts/reform/slc/services/steps/maptoletter/exceptions/InvalidMessageException.java
+++ src/main/java/uk/gov/hmcts/reform/slc/services/steps/maptoletter/exceptions/InvalidMessageException.java
@@ -1,12 +1,17 @@
 package uk.gov.hmcts.reform.slc.services.steps.maptoletter.exceptions;
 
-public class InvalidMessageException extends RuntimeException {
+import uk.gov.hmcts.reform.logging.exception.AbstractLoggingException;
+import uk.gov.hmcts.reform.logging.exception.AlertLevel;
+
+import static uk.gov.hmcts.reform.slc.logging.ErrorCode.UNKNOWN;
+
+public class InvalidMessageException extends AbstractLoggingException {
 
     public InvalidMessageException(String message, Throwable cause) {
-        super(message, cause);
+        super(AlertLevel.P4, UNKNOWN, message, cause);
     }
 
     public InvalidMessageException(String message) {
-        super(message);
+        this(message, null);
     }
 }
--- src/main/java/uk/gov/hmcts/reform/slc/services/steps/sftpupload/exceptions/FtpStepException.java
+++ src/main/java/uk/gov/hmcts/reform/slc/services/steps/sftpupload/exceptions/FtpStepException.java
@@ -1,7 +1,12 @@
 package uk.gov.hmcts.reform.slc.services.steps.sftpupload.exceptions;
 
-public class FtpStepException extends RuntimeException {
+import uk.gov.hmcts.reform.logging.exception.AbstractLoggingException;
+import uk.gov.hmcts.reform.logging.exception.AlertLevel;
+
+import static uk.gov.hmcts.reform.slc.logging.ErrorCode.UNKNOWN;
+
+public class FtpStepException extends AbstractLoggingException {
     public FtpStepException(String message, Throwable cause) {
-        super(message, cause);
+        super(AlertLevel.P1, UNKNOWN, message, cause);
     }
 }
