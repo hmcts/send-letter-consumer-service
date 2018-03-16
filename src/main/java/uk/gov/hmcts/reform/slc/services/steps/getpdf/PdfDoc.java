705e34b665f0c40f53440f8b4b41d36456e0682c
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2018-03-16 12:19:49 +0000

    Clean up model (#103)
    it no longer needs to extend sshj class

--- src/main/java/uk/gov/hmcts/reform/slc/services/steps/getpdf/PdfDoc.java
+++ src/main/java/uk/gov/hmcts/reform/slc/services/steps/getpdf/PdfDoc.java
@@ -1,11 +1,6 @@
 package uk.gov.hmcts.reform.slc.services.steps.getpdf;
 
-import net.schmizz.sshj.xfer.InMemorySourceFile;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-
-public class PdfDoc extends InMemorySourceFile {
+public class PdfDoc {
 
     public final String filename;
     public final byte[] content;
@@ -13,20 +8,5 @@ public class PdfDoc extends InMemorySourceFile {
     public PdfDoc(String filename, byte[] content) {
         this.filename = filename;
         this.content = content;
-    }
-
-    @Override
-    public String getName() {
-        return this.filename;
-    }
-
-    @Override
-    public long getLength() {
-        return this.content.length;
-    }
-
-    @Override
-    public InputStream getInputStream() {
-        return new ByteArrayInputStream(this.content);
     }
 }
