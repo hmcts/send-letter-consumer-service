32ebc20dd61573091dc1971c4d8d9a8b09027260
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-03-22 10:55:32 +0000

    Fix SonarQube error on inheritance

--- src/main/java/uk/gov/hmcts/reform/slc/services/steps/sftpupload/exceptions/FtpStepException.java
+++ src/main/java/uk/gov/hmcts/reform/slc/services/steps/sftpupload/exceptions/FtpStepException.java
@@ -3,6 +3,10 @@ package uk.gov.hmcts.reform.slc.services.steps.sftpupload.exceptions;
 import uk.gov.hmcts.reform.logging.exception.AlertLevel;
 import uk.gov.hmcts.reform.logging.exception.UnknownErrorCodeException;
 
+/**
+ * SonarQube reports as error. Max allowed - 5 parents
+ */
+@SuppressWarnings("squid:MaximumInheritanceDepth")
 public class FtpStepException extends UnknownErrorCodeException {
     public FtpStepException(String message, Throwable cause) {
         super(AlertLevel.P1, message, cause);
