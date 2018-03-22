8686412732e7c1cb0e88e50cf3cbee88eadf5c49
donatas M <doncem@users.noreply.github.com>
2018-03-22 10:30:14 +0000

    Introduce RootController (#108)

---
+++ src/test/java/uk/gov/hmcts/reform/slc/controllers/GetWelcomeTest.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.reform.slc.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class GetWelcomeTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_welcome_upon_root_request_with_200_response_code() throws Exception {
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("Welcome to Send Letter Consumer"));
+    }
+}
