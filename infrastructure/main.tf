4b4a8819934de9c84b33f37275cbb8f5b713352e
leszekg <gonczar@gmail.com>
2018-03-23 12:37:53 +0000

    Add product's Azure Vault to terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -64,3 +64,14 @@ module "consumer" {
     FTP_PUBLIC_KEY            = "${replace(data.vault_generic_secret.ftp_public_key.data["value"], "\\n", "\n")}"
   }
 }
+
+module "key-vault" {
+  source              = "git@github.com:contino/moj-module-key-vault?ref=master"
+  product             = "${var.product}"
+  env                 = "${var.env}"
+  tenant_id           = "${var.tenant_id}"
+  object_id           = "${var.jenkins_AAD_objectId}"
+  resource_group_name = "${var.product}-producer-${var.env}"
+  # dcd_cc-dev group object ID
+  product_group_object_id = "38f9dea6-e861-4a50-9e73-21e64f563537"
+}
