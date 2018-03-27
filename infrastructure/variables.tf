49531a29c866cf5affb17b039c7342b897369de7
leszekg <gonczar@gmail.com>
2018-03-27 16:40:59 +0100

    Add additional_host_name terraform variable

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -72,4 +72,8 @@ variable "jenkins_AAD_objectId" {
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
+variable "additional_host_name" {
+  default = "send-letter-consumer.platform.hmcts.net"
+}
+
 # endregion
